9dd3ba7cc056011ada38afe1f01895d0005fe3fe
Unknown <abraham.riedel@gmail.com>
2018-06-05 15:25:41 -0400

    Changed main,py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import scraper.scraper as scraper
 import logging
 import os
+from constants import DATABASE_FILE
+from scraper import server_helper as helper
 
 INPUT_FILE = 'package_names.csv'
 
@@ -13,15 +15,20 @@ def main(logger):
     if not os.path.isfile(INPUT_FILE):
         logger.error("Cannot find the provided input file")
         return
-
+    """
     logging.info("Reading the input file...")
     package_names = pd.read_csv(INPUT_FILE, names=['package_name'])['package_name']
     logging.info("Found {} entries in the file".format(package_names.size))
     chunks = [package_names[x:x + 10].tolist() for x in range(0, len(package_names), 10)]
+    s = scraper.Scraper(INPUT_FILE=INPUT_FILE, DATABASE_FILE=DATABASE_FILE)
     for chunk in chunks:
-        scraper.scrape_metadata_for_apps(chunk)
+        s.scrape_metadata_for_apps(chunk)
     return
-
+    """
+    proc = helper.start_scraper_server()
+    s = scraper.Scraper(INPUT_FILE=INPUT_FILE, DATABASE_FILE=DATABASE_FILE)
+    s.full_scrape_metadata_for_apps()
+    helper.stop_scraper_server(proc)
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s [%(name)-12.12s] %(levelname)-8s %(message)s',
