b2f58e442124e50b776e770b60377e3f0e0d9f9e
aried02 <abraham.riedel@gmail.com>
2018-06-05 15:42:43 -0400

    Scraper Modularized (#6)
    Refactoring the Scraper module to be more modular
    
    * Modularizing Scraper and server_helper fixes
    
    Includes all changes for scraper and server_helper
    
    * Small fixes
    
    * Changed main.py
    
    * chore: Refactor code
    
    Removed comments and reformat the file
    
    * chore: Refactoring

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import pandas as pd
 import scraper.scraper as scraper
 import logging
 import os
+from constants import DATABASE_FILE
+from scraper import server_helper as helper
 
 INPUT_FILE = 'package_names.csv'
 
@@ -13,14 +14,10 @@ def main(logger):
     if not os.path.isfile(INPUT_FILE):
         logger.error("Cannot find the provided input file")
         return
-
-    logging.info("Reading the input file...")
-    package_names = pd.read_csv(INPUT_FILE, names=['package_name'])['package_name']
-    logging.info("Found {} entries in the file".format(package_names.size))
-    chunks = [package_names[x:x + 10].tolist() for x in range(0, len(package_names), 10)]
-    for chunk in chunks:
-        scraper.scrape_metadata_for_apps(chunk)
-    return
+    proc = helper.start_scraper_server()
+    s = scraper.Scraper(input_file=INPUT_FILE, database_file=DATABASE_FILE)
+    s.full_scrape_metadata_for_apps()
+    helper.stop_scraper_server(proc)
 
 
 if __name__ == '__main__':
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,20 +1,100 @@
 import pandas as pd
-from constants import CATEGORIES, DATABASE_FILE
+from constants import CATEGORIES
 import logging
 import scraper.server_helper as helper
 import scraper.uuid_generator as uuid_generator
 import time
+from constants import DATABASE_FILE
 
 logger = logging.getLogger(__name__)
 
 
-def get_downloaded_app_counts():
-    main_data = pd.read_csv(DATABASE_FILE)
-    logger.info("\n" + str(main_data['genreId'].value_counts()))
-    counts = [0] * len(CATEGORIES)
-    downloaded_app_counts = pd.Series(counts, index=CATEGORIES)
-    downloaded_app_counts.update(main_data['genreId'].value_counts())
-    return downloaded_app_counts
+class Scraper:
+    """
+    Class represented a scraper object with inputting either a list of filenames directly, or a csv file
+    containing the list, and a file to output the scraped metadata (but most functions have options to directly
+    return the dataframe as well)
+    """
+
+    def __init__(self, database_file=DATABASE_FILE, input_file=None, package_names=None):
+        self.__database_file = database_file
+        if package_names is None:
+            if input_file is None:
+                raise Exception('Invalid, need input file or package name list')
+            self.package_names = pd.read_csv(input_file, names=['package_name'])['package_name']
+        else:
+            self.package_names = package_names
+
+    def get_downloaded_app_counts(self):
+        main_data = pd.read_csv(self.__database_file)
+        logger.info("\n" + str(main_data['genreId'].value_counts()))
+        counts = [0] * len(CATEGORIES)
+        downloaded_app_counts = pd.Series(counts, index=CATEGORIES)
+        downloaded_app_counts.update(main_data['genreId'].value_counts())
+        return downloaded_app_counts
+
+    """
+    Function uses default input file to scrape all of the information for every app in the file
+    Essentially what main.py used to do, but can also return the dataframe if necessary (useful for updating)
+    """
+
+    def full_scrape_metadata_for_apps(self, return_dataframe=False, write_file=True):
+        chunks = [self.package_names[x:x + 10].tolist() for x in range(0, len(self.package_names), 10)]
+        maindf = pd.DataFrame()
+        for chunk in chunks:
+            df = self.scrape_metadata_for_apps(chunk)
+            if df is None:
+                break
+            maindf = maindf.append(df, sort=False)
+        maindf = remove_unwanted_columns(maindf)  # do again on whole db
+        if write_file:
+            maindf.to_csv(self.__database_file, index=False)
+        if return_dataframe:
+            return maindf
+
+    def scrape_metadata_for_apps(self, apps):
+        logger.info("Collecting metadata for - %s" % apps)
+        urls = ["http://localhost:3000/api/apps/%s" % app for app in apps]
+        meta_data = helper.get_app_information(urls)
+        if len(meta_data) <= 0:
+            return
+        new_rows = remove_unwanted_columns(pd.DataFrame(meta_data))
+        new_rows['fileName'] = uuid_generator.generate_uuids(len(new_rows))
+        new_rows['date_metadata_collected'] = time.time()
+        return new_rows
+        # new_rows.to_csv(self.DATABASE_FILE, mode='a', index=False, header=False)
+        # logger.info("Saved metadata to file - %s" % new_rows['appId'].tolist())
+
+    def scrape_top_free_from_categories(self):
+        downloaded_app_counts = self.get_downloaded_app_counts()
+        # CATEGORY list incomplete
+        df = pd.DataFrame()
+        for category in CATEGORIES:
+            start = downloaded_app_counts[category]
+            if start > 500:
+                continue
+            base_url = 'http://localhost:3000/api/apps?collection?topselling_free&start={}&num={}&category={}'.format(
+                start, 1, category)
+            data = helper.get_data_from_server(base_url)
+
+            # Keep collecting meta-data till no more app links are available
+            while True:
+                if data is None:
+                    break
+                results = data['results']
+                app_urls = [x['url'] for x in results]
+
+                apps = helper.get_app_information(app_urls)
+                new_rows = remove_unwanted_columns(pd.DataFrame(apps))
+                df = df.append(new_rows, sort=False)
+
+                logger.info("Category: {}\tRank:{}".format(category, start))
+                start += 1
+                if 'next' not in data:
+                    break
+                data = helper.get_data_from_server(data['next'])
+
+        df.to_csv(self.__database_file, index=False)
 
 
 def remove_unwanted_columns(df):
@@ -29,48 +109,8 @@ def remove_unwanted_columns(df):
     return df
 
 
-def scrape_metadata_for_apps(apps):
-    urls = ["http://localhost:3000/api/apps/%s" % app for app in apps]
-    meta_data = helper.get_app_information(urls)
-    if len(meta_data) <= 0:
-        return
-    new_rows = remove_unwanted_columns(pd.DataFrame(meta_data))
-    new_rows['filename'] = uuid_generator.generate_uuids(len(new_rows))
-    new_rows['date_metadata_collected'] = [time.time()] * len(new_rows)
-    new_rows.to_csv(DATABASE_FILE, mode='a', index=False, header=False)
-    logger.info("Saved metadata to file - %s" % new_rows['appId'].tolist())
-
-
-def scrape_top_free_from_categories():
-    downloaded_app_counts = get_downloaded_app_counts()
-
-    for category in CATEGORIES:
-        start = downloaded_app_counts[category]
-        if start > 500:
-            continue
-        base_url = 'http://localhost:3000/api/apps?collection?topselling_free&start={}&num={}&category={}'.format(
-            start, 1, category)
-        data = helper.get_data_from_server(base_url)
-
-        # Keep collecting meta-data till no more app links are available
-        while True:
-            if data is None:
-                break
-            results = data['results']
-            app_urls = [x['url'] for x in results]
-
-            apps = helper.get_app_information(app_urls)
-            new_rows = remove_unwanted_columns(pd.DataFrame(apps))
-            new_rows.to_csv(DATABASE_FILE, mode='a', index=False, header=False)
-
-            logger.info("Category: {}\tRank:{}".format(category, start))
-            start += 1
-            if 'next' not in data:
-                break
-            data = helper.get_data_from_server(data['next'])
-
-
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                         level=logging.INFO)
-    scrape_top_free_from_categories()
+    s = Scraper()
+    s.scrape_top_free_from_categories()
