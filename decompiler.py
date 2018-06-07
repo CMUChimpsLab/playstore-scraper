143f5ad359ca447d2e4de07de4c764f17594db5e
Kunal Bhuwalka <kunal.bhuwalka@hotmail.com>
2018-06-07 09:51:00 -0400

    review: Minor tweaks to the code and refactoring

--- decompiler.py
+++ decompiler.py
@@ -1,6 +1,5 @@
 import subprocess
 import os
-from datetime import datetime
 import time
 from constants import DECOMPILE_FOLDER, DATABASE_FILE, DOWNLOAD_FOLDER
 import logging
@@ -9,16 +8,20 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 
+
+# TODO Create a script for checking if apktool has been installed or not, and install it properly if required
+# Using a local version of apktool might be a better idea (inside the project as opposed to a system installation)
 class Decompiler:
     """
-    Decompiles already downloaded apks into their versions, with the same
-    filename with the .apk extension dropped
-    1. use_database (default=True): use database_file to get filenames of apps
-    2. database_file: file to grab filenames from if provided
+    Decompiles apps provided as parameters into folders with the same filename as the apps but without the extension
+    1. use_database (default=True): use database_file to get file names of apps
+    2. database_file: file to grab file names from if provided
     3. decompile_folder: folder to decompile the apps into, existing decompiled apps will **not** be overwritten unless told to)
-    4. download_folder: location of the already downloaded apks
+    4. download_folder: folder containing the apps to decompile
     """
-    def __init__(self, use_database=True, database_file=DATABASE_FILE, decompile_folder=DECOMPILE_FOLDER, download_folder=DOWNLOAD_FOLDER):
+
+    def __init__(self, use_database=True, database_file=DATABASE_FILE, decompile_folder=DECOMPILE_FOLDER,
+                 download_folder=DOWNLOAD_FOLDER):
         self.__decompile_folder = decompile_folder
         self.__download_folder = download_folder
 
@@ -33,53 +36,54 @@ class Decompiler:
             self.__database_file = database_file
             self.__database_helper = dbhelper.DatabaseHelper(self.__database_file)
 
-
     def decompile(self, file_names, force_decompile=False):
         """
         Decompiles the files specified
-        :param file_names: list of filenames to decompile (if doesn't include .apk extension, will be added), gets overwritten if using the database
-        :param force_decompile: will forcibly overwrite and re-decompile files whose name already appears in "decompile_folder"
+        :param file_names: list of file names to decompile (if doesn't include .apk extension, will be added), this list
+        gets overwritten if the database is used
+        :param force_decompile: will forcibly overwrite and re-decompile files whose name already
+        appears in "decompile_folder"
         :return: list of decompile times and None entries for unsuccessful decompiles
         """
         if self.__use_database:
             file_names = self.__database_helper.get_filename_mappings(file_names)
             file_names = [x[1] for x in file_names]
-        
-        if file_names[0][-4:] != '.apk':
-            file_names = [f + '.apk' for f in file_names]
 
-        decompiled_apps=os.listdir(self.__decompile_folder)
-        downloaded_apps=os.listdir(self.__download_folder)
+        app_extension = '.apk'
+        file_names = [f if f.endswith(app_extension) else f + app_extension for f in file_names]
+
+        decompiled_apps = os.listdir(self.__decompile_folder)
+        downloaded_apps = os.listdir(self.__download_folder)
         decompile_completion_time = []
-        decompath = self.__decompile_folder+"/"
-        downpath = self.__download_folder+"/"
-        logger.info("Decompiling into - %s" % decompath)
-        
+        logger.info("Decompiling into - %s" % self.__decompile_folder)
+
         for fname in file_names:
-            if not force_decompile and fname[:-4] in decompiled_apps:
+            if not force_decompile and fname[:-len(app_extension)] in decompiled_apps:
                 logger.info("File %s already decompiled" % fname)
                 continue
-            
+
             if fname not in downloaded_apps:
                 logger.error("File %s not found" % fname)
                 continue
             try:
-                cmd = "apktool d "+downpath+fname+" -o "+decompath+fname[:-4]
+                app_file_path = '/'.join([self.__download_folder, fname])
+                decompile_destination_path = '/'.join([self.__decompile_folder, fname[:-len(app_extension)]])
+                cmd = "apktool d {} -o {}".format(app_file_path, decompile_destination_path)
                 with open(os.devnull, 'w') as fp:
                     p = subprocess.run(cmd.split(), stdout=fp, stderr=fp)
                 if not p or p.returncode != 0:
                     logger.error("Nonzero exit code received for %s" % fname)
                     decompile_completion_time.append(None)
                 else:
-                    logger.info("Decompiled {} into {}".format(downpath+fname, decompath+fname[:-4]))
+                    logger.info("Decompiled {} into {}".format(app_file_path, decompile_destination_path))
                     decompile_completion_time.append(time.time())
             except Exception as e:
-                logger.error("Decompile failed - fname %s" % fname)
+                logger.error("Decompile failed - %s" % fname)
                 logger.error(e)
                 decompile_completion_time.append(None)
-        
+
         return decompile_completion_time
-        
+
     def decompile_apps_from_file(self, filename):
         """
         Decompile apps with file names specified in the CSV file passed as a parameter
@@ -100,6 +104,7 @@ class Decompiler:
             return [None]
         apps = df['fileName'].tolist()
         return self.decompile(apps)
+
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s [%(name)-12.12s] %(levelname)-8s %(message)s',
--- test_decompiler.py
+++ test_decompiler.py
@@ -5,7 +5,7 @@ from scraper.scraper import Scraper
 from scraper import server_helper as helper
 import os
 import shutil
-import logging
+
 
 class TestDecompiler(TestCase):
     def setUp(self):
@@ -13,11 +13,10 @@ class TestDecompiler(TestCase):
         self.__decompile_folder = "test_decompile_folder"
         self.__test_app_name = 'com.facebook.katana'
         self.__downloader = Downloader(use_database=False, download_folder=self.__download_folder)
-        self.__decompiler = Decompiler(use_database=False, download_folder=self.__download_folder, decompile_folder=self.__decompile_folder)
+        self.__decompiler = Decompiler(use_database=False, download_folder=self.__download_folder,
+                                       decompile_folder=self.__decompile_folder)
         self.__test_csv_file = 'test_file'
-        logging.basicConfig(format='%(asctime)s [%(name)-12.12s] %(levelname)-8s %(message)s',
-                            level=logging.INFO)
-        
+
     def tearDown(self):
         shutil.rmtree(self.__download_folder)
         shutil.rmtree(self.__decompile_folder)
@@ -25,7 +24,7 @@ class TestDecompiler(TestCase):
             os.remove(self.__test_csv_file)
         if os.path.exists('test_data.csv'):
             os.remove('test_data.csv')
-    
+
     def test_decompile_folder_creation(self):
         self.assertTrue(os.path.isdir(self.__decompile_folder))
 
@@ -34,7 +33,7 @@ class TestDecompiler(TestCase):
         return_value = self.__decompiler.decompile([self.__test_app_name])
         self.assertTrue(os.path.isdir(self.__decompile_folder + '/com.facebook.katana'))
         self.assertEqual(len(return_value), 1)
-    
+
     def test_decompile_with_extension(self):
         self.__downloader.download([[self.__test_app_name, 'test.apk']])
         return_value = self.__decompiler.decompile(['test.apk'])
@@ -62,8 +61,7 @@ class TestDecompiler(TestCase):
         helper.stop_scraper_server(proc)
         new_downloader = Downloader(database_file='test_data.csv', download_folder=self.__download_folder)
         new_downloader.download(None)
-        new_decompiler = Decompiler(use_database=True, database_file='test_data.csv', download_folder=self.__download_folder, decompile_folder=self.__decompile_folder)
+        new_decompiler = Decompiler(use_database=True, database_file='test_data.csv',
+                                    download_folder=self.__download_folder, decompile_folder=self.__decompile_folder)
         new_decompiler.decompile(None)
         self.assertTrue(os.listdir(self.__decompile_folder) != [])
-
-
