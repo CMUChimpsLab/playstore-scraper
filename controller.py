dc3465d9cd9be2d32ad5f3277cf8c51f23507e76
Unknown <abraham.riedel@gmail.com>
2018-06-25 15:15:51 -0400

    Changed some controller, added chunking to test.py
    Chunking so that we don't have to scrape every single app then update, yay!

--- controller.py
+++ controller.py
@@ -30,27 +30,44 @@ class Controller:
             for i in l:
                 down_and_analyze(i)
         os.system("rm -r txt_files") #just to ensure it's cleaned
+
+def to_file_for_analysis(uuid_list):
+    # Writes a file with appropriate format to feed to analysis pipeline
+    # :param uuid_list: List of uuids to analyze (without apk extension)
+    # Returns the file name of the file written to
     
-def down_and_analyze(name):
-    downloader = Downloader()
-    decompiler = Decompiler(use_database=False)
-    dbhelper = DbHelper()
-    logger.info("Downloading App %s" % name)
-    uuid_list = downloader.download([name])
-    # Now decompile them
-    logger.info("Decompiling App %s" % name)
-    decompiler.decompile(uuid_list)
-    # Take a uuid, then write to a file
     fname = str(name_gen.uuid4())+'.txt'
     local_directory = os.path.dirname(os.path.realpath(__file__))
     local_directory = local_directory[:local_directory.index('playstore-scraper') + len('playstore-scraper')]
     # print("/".join[local_directory, DOWNLOAD_FOLDER])
     with open("txt_files/"+fname, 'w') as f:
         for uuid in uuid_list:
-            f.write(uuid+'.apk' + ' ' + "/".join([local_directory, DOWNLOAD_FOLDER]))
+            if not uuid.endswith('apk'):
+                uuid = uuid+'.apk'
+            f.write(uuid + ' ' + "/".join([local_directory, DOWNLOAD_FOLDER]))
+    return fname
+
+def down_and_analyze(name):
+    downloader = Downloader()
+    decompiler = Decompiler(use_database=False)
+    dbhelper = DbHelper()
+
+    logger.info("Downloading App %s" % name)
+    uuid_list = downloader.download([name])
+    
+    # Now decompile them
+    logger.info("Decompiling App %s" % name)
+    decompiler.decompile(uuid_list)
+    
+    # Take a uuid, then write to a file
+    fname = to_file_for_analysis(uuid_list)
     logger.info("Ready to analyze %s" % name)
+
+    #Analyze the file (auto put results in a bad database)
     os.system("python2.7 pipeline/python_static_analyzer/main_LargeVM.py pipeline/python_static_analyzer/fake txt_files/"+fname)
     for uuid in uuid_list:
+        # Pass dbhelper and client to avoid a large amount of open connections
+        # to the database (still end up with ~30 though)
         fix(uuid, helper=dbhelper, client=dbhelper._DbHelper__client)
         logger.info("%s fixed" % name)
         dbhelper.update_analyses_done(uuid, ['3rd_party_packages', 'linkurl', 'permissionlist'])
--- test.py
+++ test.py
@@ -1,10 +1,25 @@
 import logging
 from scraper.scraper import Scraper
-from Controller import Controller
-logging.basicConfig(format='%(asctime)s [%(name)-12.12s] %(levelname)-8s %(message)s',
-                        level=logging.INFO)
-s = Scraper(input_file='package_names.csv')
-s.scrape_metadata_for_apps()
-c = Controller()
-c.down_all_apps(True)
+from controller import Controller
+import pandas as pd
 
+def main_chunk(fname):
+    package_names = pd.read_csv(fname, names=['package_name'])['package_name'].tolist()
+    s = Scraper()
+    c = Controller()
+    # make chunks divisible by 4
+    chunks = [package_names[x : x+16] for x in range(0, len(package_names), 16)]
+    for chunk in chunks:
+        s.scrape_metadata_for_apps(package_names=chunk)
+        c.down_all_apps(True)
+
+def main_all(fname):
+    s = Scraper(input_file=fname)
+    s.scrape_metadata_for_apps()
+    c = Controller()
+    c.down_all_apps(True)
+
+if __name__=='__main__':
+    logging.basicConfig(format='%(asctime)s [%(name)-12.12s] %(levelname)-8s %(message)s',
+                        level=logging.INFO)
+    main_chunk('package_names.csv')
