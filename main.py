ee2e654abf7e5ab897900ced27719e43922f6869
Unknown <abraham.riedel@gmail.com>
2018-07-18 12:49:50 -0400

    More options using main
    Added some CL functionality with main function.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import logging
 from scraper.scraper import Scraper
 from controller import Controller
@@ -16,9 +16,17 @@ def download_all():
     d = Downloader()
     d.download_all_from_db()
     
-def scrape(package_names):
+def scrape_names(package_names):
     s = Scraper()
     s.scrape_metadata_for_apps(package_names=package_names)
+
+def scrape_file(fname):
+    s = Scraper(input_file=fname)
+    s.scrape_metadata_for_apps()
+
+def update():
+    u = Updater()
+    u.update_apps_bulk()
 
 def download_and_decompile():
     # implement later
@@ -31,9 +39,7 @@ def update_top_list():
     d = DbHelper()
     d.update_top_apps()
 
-def update_test():
-    u = Updater()
-    u.update_apps_bulk()
+
 
 def put_top_apps_in_db():
     d = DbHelper()
@@ -44,6 +50,23 @@ def put_top_apps_in_db():
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s [%(name)-12.12s] %(levelname)-8s %(message)s',
                         level=logging.INFO)
-    download_all()
-    scrape(['com.metago.astro'])
-    download_all()
+    opt = sys.argv[1]
+    if opt == 's': #scrape
+        if len(sys.argv) <= 2:
+            print("Must supply csv with package names for scraping")
+            sys.exit(1)
+        fname = sys.argv[2]
+        scrape_file(fname)
+    
+    elif opt == 'u': #update from database (using bulk update for speed)
+        update()
+    
+    elif opt == 'down': #download
+        download_all()
+    
+    elif opt == 'decom':
+        return #TODO
+    
+    elif opt == 'dd': #download and decompile
+        download_and_decompile()
+    
