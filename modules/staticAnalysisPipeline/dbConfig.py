22d1e1eb7523c878d52dfa4a33eb1e8f021ecfe8
Alex Yu <alexyu0@ip-172-30-2-135.ec2.internal>
2018-08-30 02:29:36 +0000

    hotfix

--- modules/staticAnalysisPipeline/dbConfig.py
+++ modules/staticAnalysisPipeline/dbConfig.py
@@ -1,5 +1,5 @@
 #Do not commit login info to github !!
 #There is security risk to leak the ip address of our mongodb server
-HOSTNAME = "128.0.0.1"
-USERNAME = "analyzer"
-PASSWORD = "iamanalyzer007"
+HOSTNAME = "XXX.XXX.XXX.XXX"
+USERNAME = "XXXXX"
+PASSWORD = "XXXXX"
