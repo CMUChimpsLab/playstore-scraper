802264273d36544bc10162a3d0a1d9a00130bc43
privacy <privacy@app-analysis.andrew.cmu.edu>
2018-09-08 17:15:30 +0000

    updated pipeline testing

--- modules/staticAnalysisPipeline/pipeline.sh
+++ modules/staticAnalysisPipeline/pipeline.sh
@@ -4,7 +4,7 @@ set -e
 # If you uncomment all the following lines, it will be a whole round of data updating process.
 
 # However, sometimes it breaks at some points, so uncomment/commment based on which steps you are in
-# This time stamp is an identifier for each round of APK updating. 
+# This time stamp is an identifier for each round of APK updating.
 # It is not a identifier for static anlaysis, you may conduct several rounds of static anlaysis.
 #Now=$(date +"%Y%m%d%H%M")
 
@@ -25,6 +25,8 @@ NEW_APKS="$CURR_DIR/new_apks.txt" # where apks to analyze are listed
 # cleans up from broken update or killed program
 clean_up () {
     ARG=$?
+    mv $OUTPUT_PATH/filelist.txt .
+    mv $OUTPUT_PATH/exceptions.log .
     rm -rf temp $NEW_APKS $OUTPUT_PATH
     echo "cleaned up"
 }
@@ -34,21 +36,18 @@ trap clean_up SIGINT
 # First line of the log is timestamp
 date >> $LOG_DIR/pipeline_$Now
 
-# rsync APKs from NAS 
+# rsync APKs from NAS
 # TODO handle separation of analyzed and unanalyzed apps
 mkdir -p $CURR_DIR/temp
-rsync -Pa -i --log-file=$NEW_APKS \
-    -e "ssh -i ~/.ssh/alexyu0-rsa" \
-    alexyu0@128.2.115.102:/volume1/Project-PrivacyProxy/all_apps/eb80a8887bbe4e4f889da44e80143dff \
-    temp
-dir=$(pwd)
-sed '1d;$d;s@$@ '"$dir/temp"'@' $NEW_APKS | awk 'END {print $(NF-1), $NF > "'$NEW_APKS'"}'
+filename="eb8d16c3b1d3406383fb16db9b1ed8ff.apk"
+echo "$filename /home/privacy/nas/apps" > $NEW_APKS
 
-# Static analysis of all apks 
+# Static analysis of all apks
 # TODO parallelize this again
 mkdir -p $OUTPUT_PATH
-python $DEP_DIR/python_static_analyzer/main_LargeVM.py $OUTPUT_PATH
+python $DEP_DIR/python_static_analyzer/main_LargeVM.py $OUTPUT_PATH $NEW_APKS
 echo "static analysis completed at `date`" >> $LOG_DIR/pipeline_$Now
+cat $OUTPUT_PATH/filelist.txt
 
 # static analysis will output a file named `filelist.txt`.
 cp $OUTPUT_PATH/filelist.txt $DEP_DIR/privacyRating/data/updatedApkList/updatedApkList_$Now
@@ -56,9 +55,11 @@ echo "copying updatedApkList to privacyRating data folder completed at `date`" >
 
 # extract permission and purpose, then give scores & grades based on grading model in `privacyRating/src/rateApp.py`
 echo "this step"
+echo "$DEP_DIR"
+cat $OUTPUT_PATH/filelist.txt
 python $DEP_DIR/privacyRating/src/extractApp.py \
-    $DEP_DIR \
-    $DEP_DIR/privacyRating/data/updatedApkList/updatedApkList_$Now \
+    "$DEP_DIR" \
+    $DEP_DIR/privacyRating/data/updatedApkList/updatedApkList_$Now
 echo "completed at rating apps at `date`" >> $LOG_DIR/pipeline_$Now
 
 #Remember to backup mongodb database: androidApp, staticAnalysis, privacygrading, after you finish one round of updating
