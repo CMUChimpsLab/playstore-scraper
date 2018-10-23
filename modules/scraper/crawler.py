9bb11e3666eb2929cd2f8f65cf674e1fbe47f3a2
Alex Yu <alexyu0925@gmail.com>
2018-10-23 18:41:25 -0400

    multithreaded crawling

--- modules/scraper/crawler.py
+++ modules/scraper/crawler.py
@@ -1,8 +1,9 @@
 import logging
 import time
 import urllib
+from concurrent.futures import ThreadPoolExecutor
 
-from dependencies.constants import CATEGORIES
+from dependencies.constants import CATEGORIES, THREAD_NO
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -28,42 +29,49 @@ def get_top_apps_list():
     store will not allow 'start' to be > 199 and will not allow 'num' to be
     > 320
     """
-    url = 'https://play.google.com/store/apps/category/{}/collection/topselling_{}?hl=en&gl=us&start={}&num={}'
+    pkg_list = []
+    logger.info("Starting crawl")
+    with ThreadPoolExecutor(max_workers=THREAD_NO) as executor:
+        res = executor.map(top_app_crawl_thread_worker, CATEGORIES)
+        for r in res:
+            pkg_list.extend(r)
+
+    return list(set(pkg_list))
+
+def top_app_crawl_thread_worker(cat):
     # have to get 0 -> 100 then 100 -> 199 then 199 -> 318 (google doesn't like
     # num to be > 120 and start to be > 199. Idk why.
     starts = [0, 120, 199]
     nums = [120, 79, 120]
     free = ['free', 'paid']
-    pkg_list = []
-    logger.info("Starting crawl")
-    for cat in CATEGORIES:
-        for f in free:
-            for start, num in zip(starts, nums):
-                newurl = url.format(cat, f, start, num)
-                while True:
-                    try:
-                        l = urllib.request.urlopen(newurl).read()
-                        l = l.decode('utf-8')
-                        p = scrape(l)
-                        logger.info("Apps found for category {} is {}".format(cat, len(list(set(p)))))
+    url = 'https://play.google.com/store/apps/category/{}/collection/topselling_{}?hl=en&gl=us&start={}&num={}'
+    for f in free:
+        for start, num in zip(starts, nums):
+            newurl = url.format(cat, f, start, num)
+            while True:
+                try:
+                    l = urllib.request.urlopen(newurl).read()
+                    l = l.decode('utf-8')
+                    p = scrape(l)
+                    logger.info("Apps found for category {} is {}".format(cat, len(list(set(p)))))
 
-                        pkg_list = pkg_list + scrape(l)
-                    except urllib.error.HTTPError as e:
-                        if e.code == 404:
-                            # not valid
-                            logger.error("Category {} with {} apps not valid")
-                            continue
-                        elif e.code == 500:
-                            # no more items that match
-                            continue
-                        elif e.code == 429:
-                            # hit rate limit so sleep
-                            logger.error("hit rate limit, sleeping")
-                            time.sleep(5)
-                            continue
-                    break
+                    pkg_list = pkg_list + scrape(l)
+                except urllib.error.HTTPError as e:
+                    if e.code == 404:
+                        # not valid
+                        logger.error("Category {} with {} apps not valid")
+                        continue
+                    elif e.code == 500:
+                        # no more items that match
+                        continue
+                    elif e.code == 429:
+                        # hit rate limit so sleep
+                        logger.error("hit rate limit, sleeping")
+                        time.sleep(5)
+                        continue
+                break
 
-            pkg_list = list(set(pkg_list))
-        logger.info("Category %s done" % cat)
+        pkg_list = list(set(pkg_list))
+        logger.info("Category {} {} apps done".format(cat, f))
     return pkg_list
 
