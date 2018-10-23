c6c19e1964a6ffd5fe8a9588b0bc2beccc6b80d8
privacy <privacy@app-analysis.andrew.cmu.edu>
2018-10-23 22:36:22 +0000

    need to parallelize crawler

--- modules/scraper/crawler.py
+++ modules/scraper/crawler.py
@@ -1,11 +1,6 @@
 import logging
-
-# fix import error for when being imported by analyzer that uses 2.7
-try:
-    import urllib.request
-except ImportError:
-    import urllib
-
+import time
+import urllib
 
 from dependencies.constants import CATEGORIES
 
@@ -30,14 +25,14 @@ def get_top_apps_list():
     """
     Iterates through all app store categories and gets the top (US) apps from
     each one. Returns as a package_name list. For some reason, google play
-    store will not allow 'start' to be > 199 and will not allow num to be
+    store will not allow 'start' to be > 199 and will not allow 'num' to be
     > 320
     """
     url = 'https://play.google.com/store/apps/category/{}/collection/topselling_{}?hl=en&gl=us&start={}&num={}'
     # have to get 0 -> 100 then 100 -> 199 then 199 -> 318 (google doesn't like
     # num to be > 120 and start to be > 199. Idk why.
-    starts = [0, 100, 199]
-    nums = [100, 99, 120]
+    starts = [0, 120, 199]
+    nums = [120, 79, 120]
     free = ['free', 'paid']
     pkg_list = []
     logger.info("Starting crawl")
@@ -45,15 +40,30 @@ def get_top_apps_list():
         for f in free:
             for start, num in zip(starts, nums):
                 newurl = url.format(cat, f, start, num)
-                l = urllib.request.urlopen(newurl).read()
-                l = l.decode('utf-8')
-                p = scrape(l)
-                logger.info("Apps found for category {} is {}".format(cat, len(list(set(p)))))
+                while True:
+                    try:
+                        l = urllib.request.urlopen(newurl).read()
+                        l = l.decode('utf-8')
+                        p = scrape(l)
+                        logger.info("Apps found for category {} is {}".format(cat, len(list(set(p)))))
 
-                pkg_list = pkg_list + scrape(l)
+                        pkg_list = pkg_list + scrape(l)
+                    except urllib.error.HTTPError as e:
+                        if e.code == 404:
+                            # not valid
+                            logger.error("Category {} with {} apps not valid")
+                            continue
+                        elif e.code == 500:
+                            # no more items that match
+                            continue
+                        elif e.code == 429:
+                            # hit rate limit so sleep
+                            logger.error("hit rate limit, sleeping")
+                            time.sleep(5)
+                            continue
+                    break
+
             pkg_list = list(set(pkg_list))
         logger.info("Category %s done" % cat)
     return pkg_list
-
-
 
