ea72ebd9ba71b596551f45fe823f193963a903c1
Alex Yu <alexyu0925@gmail.com>
2019-03-20 15:45:26 -0400

    include plugin tests teardown

--- plugins/development/manifest_parser/__init__.py
+++ plugins/development/manifest_parser/__init__.py
@@ -3,4 +3,4 @@ import sys
 # sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
 from .main import run
-from .test_manifest_parser import test_load_setup
+from .test_manifest_parser import test_load_setup, teardown
--- plugins/development/manifest_parser/test_manifest_parser.py
+++ plugins/development/manifest_parser/test_manifest_parser.py
@@ -1,12 +1,21 @@
 from .main import run
+import subprocess
 
+apps = [
+    {
+        "uuid" : "5e5f7394701145fc92676714539f7041",
+        "packageName" : "com.android.chrome",
+    },
+]
 def test_load_setup():
-    apps = [
-        {
-            "uuid" : "5e5f7394701145fc92676714539f7041",
-            "packageName" : "com.android.chrome",
-        },
-    ]
-
     return (apps, run)
 
+def teardown():
+    for a in apps:
+        rm_res = subprocess.run(" ".join(["rm", "-rf", a["uuid"]]),
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+            shell=True)
+        if rm_res.returncode != 0:
+            print("teardown rm err for {}: {}".format(a["packageName"], rm_res.stderr))
+            continue
