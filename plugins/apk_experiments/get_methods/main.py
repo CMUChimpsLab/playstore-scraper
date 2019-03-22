3f4bcb89e008cf3ac54b261124e050b4de8655c6
Alex Yu <alexyu0925@gmail.com>
2019-03-21 20:51:06 -0400

    handle different AnalyzeAPK return values

--- plugins/apk_experiments/get_methods/main.py
+++ plugins/apk_experiments/get_methods/main.py
@@ -3,7 +3,7 @@ def all_methods_to_file(androguard_objs):
     Input expected to be list of (<APK>, <DalvikVM>, <uVMAnalysis>) tuples
     """
     with open("all_methods.txt", "w") as f:
-        for (a, d_list, dx_list) in androguard_objs:
+        for (a, d_list, dx) in androguard_objs:
             for d in d_list:
                 for method in d.get_methods():
                     f.write("{},{},{}\n".format(method.name, method.class_name, method.code))
@@ -14,4 +14,3 @@ def run(androguard_objs):
     (<APK>, [<DalvikVM>, ...], [<uVMAnalysis>, ...]) tuples
     """
     all_methods_to_file(androguard_objs)
-
